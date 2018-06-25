4cfac336f66d9529573a5f8ca2c89baac2720a81
Ed Summers <ehs@pobox.com>
2018-06-25 06:45:43 -0400

    new version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md") as f:
 
 setup(
     name = 'etudier',
-    version = '0.0.4',
+    version = '0.0.5',
     url = 'https://github.com/edsu/etudier',
     author = 'Ed Summers',
     author_email = 'ehs@pobox.com',
